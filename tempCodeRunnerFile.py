1aa2c630fe7d7806c832aa7d89c489d0d7185234
Krishna <krishnanoutiyal@gmail.com>
2022-10-09 19:16:54 +0530

    Waste Removal
    Removed unnecessary files

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-
-Db.commit()
-fnet = Fernet("ugAAJqBSpXQQcPZC_AC-A6zjqdxaYoYAh5-vtKhiW_g==")
-cr.execute("select * from flask;")
-
-Lst = [[i[0],Decrypt(i[1]),Decrypt(i[2])] for i in cr.fetchall()]
-
-for i in Lst:
-    print(f"{i[0]} : {i[1]} : {i[2]}")
